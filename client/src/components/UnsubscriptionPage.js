1289f68b89263e7091a51d0ddfe23c0880eab62d
Yosefc <yosef.conn@gmail.com>
2019-04-17 10:50:49 +0300

    Render elment if radio id "other" is checked

--- client/src/components/UnsubscriptionPage.js
+++ client/src/components/UnsubscriptionPage.js
@@ -3,17 +3,21 @@ import SubscriptionBox from "./SubscriptionBox";
 
 class UnsubscriptionPage extends Component {
     state = {
-        checked: ''
+        checked: false,
+        value: ""
     }
 
-    handleChange = e => {
+    handleRadioChange = e => {
+        let a = false
+        if (e.target.id === "other") a = !a
         this.setState({
-            checked: e.target.value
+            checked: a,
+            value: e.target.value
         })
     }
 
     render() {
-        const OtherReason = _ => this.state.checked === "other" ? <input type="text" placeholder="Please tell use why.." /> : null
+        const OtherReason = _ => this.state.checked ? <input type="text" placeholder="Please tell use why.." /> : null
         return (
             <div>
                 <article>
@@ -29,16 +33,16 @@ class UnsubscriptionPage extends Component {
                 <div>
                     <p>Would you mind telling us why you don't want to get email updates anymore?</p>
                     <div>
-                        <input type="radio" id="not-following" name="other" value="notFollowing" />
+                        <input type="radio" id="not-following" name="other" value="notFollowing" checked={this.state.value === "notFollowing"} onChange={this.handleRadioChange} />
                         <label htmlFor="notFollowing">I'm not following that team anymore.</label>
 
-                        <input type="radio" id="too-many" name="other" value="tooMany" />
+                        <input type="radio" id="too-many" name="other" value="tooMany" checked={this.state.value === "tooMany"} onChange={this.handleRadioChange} />
                         <label htmlFor="tooMany">Too many emails</label>
 
-                        <input type="radio" id="not-signed" name="other" value="notSiged" />
+                        <input type="radio" id="not-signed" name="other" value="notSiged" checked={this.state.value === "notSiged"} onChange={this.handleRadioChange} />
                         <label htmlFor="notSiged">I didn't sign up for this</label>
 
-                        <input type="radio" id="other" name="other" value="other" checked={this.state.checked === "other"} onChange={this.handleRadioChange} />
+                        <input type="radio" id="other" name="other" value="other" checked={this.state.value === "other"} onChange={this.handleRadioChange} />
                         <label htmlFor="other">Other</label>
                         {<OtherReason />}
                     </div>
