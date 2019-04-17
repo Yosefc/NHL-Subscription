ef166f4e2189027c93f81020f33f49f11db8ae72
Yosefc <yosef.conn@gmail.com>
2019-04-17 10:53:39 +0300

    Refactored path to home page

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <Navbar />
           <main className="container">
             <img src="http://www.stickpng.com/assets/images/5a4fbb7bda2b4f099b95da15.png" width="100" height="100" alt="Logo" />
-            <Route path="/home" component={SubscriptionPage} />
+            <Route exact path="/" component={SubscriptionPage} />
             <Route path="/unsubscribe" component={UnsubscriptionPage} />
           </main>
         </div>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <header className="App-header">
             <nav>
-                <Link to="/home" className="nav-item">Home</Link>
+                <Link to="/" className="nav-item">Home</Link>
                 <Link to="/unsubscribe" className="nav-item">Unsubscribe</Link>
             </nav>
         </header>
