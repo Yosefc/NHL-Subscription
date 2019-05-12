5fd5040f1a3657236b166a5b0fcad05f40675125
Yosefc <yosef.conn@gmail.com>
2019-05-12 15:27:51 +0300

    Set up method to handle unsubscribe reason

--- client/src/components/UnsubscriptionPage.js
+++ client/src/components/UnsubscriptionPage.js
@@ -7,12 +7,12 @@ class UnsubscriptionPage extends Component {
         value: ""
     }
 
-    handleRadioChange = e => {
-        let a = false
-        if (e.target.id === "other") a = !a
+    handleRadioChange = event => {
+        let checked = false
+        if (event.target.id === "other") checked = !checked
         this.setState({
-            checked: a,
-            value: e.target.value
+            checked,
+            value: event.target.value
         })
     }
 
@@ -20,40 +20,43 @@ class UnsubscriptionPage extends Component {
         const OtherReason = _ => this.state.checked ? <input type="text" placeholder="Please tell use why.." /> : null
         return (
             <div>
-                <article>
+                <section>
                     <p>
                         Oh man...
                     <br />
                         It's us, wasn't it..
                 </p>
-                </article>
+                </section>
                 <SubscriptionBox path={window.location.pathname} />
                 <input type="radio" id="huey" name="drone" value="huey" />
                 <label htmlFor="huey">Unsubscribe me from all team updates.</label>
-                <div>
+                <div className="unsubscribe-reasons">
                     <p>Would you mind telling us why you don't want to get email updates anymore?</p>
                     <div>
-                        <input type="radio" id="not-following" name="other" value="notFollowing" checked={this.state.value === "notFollowing"} onChange={this.handleRadioChange} />
-                        <label htmlFor="notFollowing">I'm not following that team anymore.</label>
-
-                        <input type="radio" id="too-many" name="other" value="tooMany" checked={this.state.value === "tooMany"} onChange={this.handleRadioChange} />
-                        <label htmlFor="tooMany">Too many emails</label>
-
-                        <input type="radio" id="not-signed" name="other" value="notSiged" checked={this.state.value === "notSiged"} onChange={this.handleRadioChange} />
-                        <label htmlFor="notSiged">I didn't sign up for this</label>
-
-                        <input type="radio" id="other" name="other" value="other" checked={this.state.value === "other"} onChange={this.handleRadioChange} />
-                        <label htmlFor="other">Other</label>
-                        {<OtherReason />}
+                        <div>
+                            <label>
+                                <input type="radio" id="not-following" name="other" value="notFollowing" checked={this.state.value === "notFollowing"} onChange={this.handleRadioChange} />
+                                I'm not following that team anymore.
+                            </label>
+                        </div>
+                        <div>
+                            <label> <input type="radio" id="too-many" name="other" value="tooMany" checked={this.state.value === "tooMany"} onChange={this.handleRadioChange} />
+                                Too many emails</label>
+                        </div>
+                        <div>
+                            <label> <input type="radio" id="not-signed" name="other" value="notSiged" checked={this.state.value === "notSiged"} onChange={this.handleRadioChange} />
+                                I didn't sign up for this</label>
+                        </div>
+                        <div>
+                            <label><input type="radio" id="other" name="other" value="other" checked={this.state.value === "other"} onChange={this.handleRadioChange} />
+                                Other</label>
+                            {<OtherReason />}
+                        </div>
                     </div>
                 </div>
             </div>
         );
     }
 }
-
-//const OtherReason = _ =>  this.state.checked ? <input type="text" placeholder="Please tell use why.." /> : null
-
-// const OtherReason = state => { if (state.checked) { console.log("state.checked"); return <input type="text" placeholder="Please tell use why.." /> } else { return null } }
 
 export default UnsubscriptionPage
