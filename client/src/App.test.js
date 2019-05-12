c2f12cb101d5f6531b4c5ee26e5770121b6325cd
Yosefc <yosef.conn@gmail.com>
2019-05-12 15:26:04 +0300

    Added test for RouterControler

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { SubscrtionRoute } from "./Router/RouterControler";
+import { BrowserRouter } from "react-router-dom";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should render with Subscrtion route component', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<BrowserRouter><SubscrtionRoute /></BrowserRouter>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Router module should render.', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<BrowserRouter><SubscrtionRoute /></BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
