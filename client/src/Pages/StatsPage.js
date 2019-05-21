c2e022e3cfe7b1e37636905e8f22273cd9c3e49d
Yosefc <yosef.conn@gmail.com>
2019-05-21 12:56:50 +0300

    Added chart example

--- client/src/Pages/StatsPage.js
+++ client/src/Pages/StatsPage.js
@@ -1,10 +1,11 @@
 import React from "react";
+import StatsChart from "../components/StatsChart";
 
 const SatsPage = () => {
     return (
         <div>
-            Hi
-      </div>
+            <StatsChart />
+        </div>
     )
 }
 
