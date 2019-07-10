81611599587cd933f66ccd5c5d67755a55e7ceef
Yosefc <yosef.conn@gmail.com>
2019-07-10 12:06:33 +0300

    Changed logic to display correct button text

--- client/src/components/SubscriptionBox.js
+++ client/src/components/SubscriptionBox.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React from "react";
 import { getTeams } from "../teams";
 
 const SubscriptionBox = ({ path }) => {
-    const btnText = _ => path.match(/home/) ? "Subscribe" : "Unsubcribe"
-    return (
+  const btnText = _ => (path.match(/unsubscribe/) ? "Unsubcribe" : "Subscribe");
+  return (
+    <div>
+      <form>
         <div>
-            <form>
-                <div>
-                    <select>
-                        <option value="Choose Teams">Choose Teams</option>
-                        <SelectOptions />
-                    </select>
-                </div>
-                <input type="email" placeholder="Enter email" />
-                <button>{btnText()}</button>
-            </form>
+          <select>
+            <option value="Choose Teams">Choose Teams</option>
+            <SelectOptions />
+          </select>
         </div>
-    )
-}
+        <input type="email" placeholder="Enter email" />
+        <button>{btnText()}</button>
+      </form>
+    </div>
+  );
+};
 
-const SelectOptions = () => getTeams().map(team => <option key={team.id} value={team.id}>{team.name}</option>);
+const SelectOptions = () =>
+  getTeams().map(team => (
+    <option key={team.id} value={team.id}>
+      {team.name}
+    </option>
+  ));
 
-export default SubscriptionBox
+export default SubscriptionBox;
