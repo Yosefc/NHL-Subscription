3c31819c5127e79254176230955c62ef5e27a175
Yosefc <yosef.conn@gmail.com>
2019-07-11 16:11:49 +0300

    Added some design for main container

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { SubscrtionRoute, UnsubscribeRoute, StatsPageRoute } from "./Router/RouterControler";
-import './App.css';
-import Navbar from "./components/Navbar"
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   render() {
@@ -11,7 +11,12 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <main className="container">
-            <img src="http://www.stickpng.com/assets/images/5a4fbb7bda2b4f099b95da15.png" width="100" height="100" alt="Logo" />
+            <img
+              src="http://www.stickpng.com/assets/images/5a4fbb7bda2b4f099b95da15.png"
+              width="100"
+              height="100"
+              alt="Logo"
+            />
             <SubscrtionRoute />
             <UnsubscribeRoute />
             <StatsPageRoute />
