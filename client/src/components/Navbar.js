7cde73b5634852d9853cb5d40cbaa71fa86b2f3d
Yosefc <yosef.conn@gmail.com>
2019-07-11 16:01:56 +0300

    Designed the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [currentURLNavItem, setURLNavItem] = useState(window.location.pathname);
   return (
     <header className="App-header">
       <nav className="nav-bar">
-        <Link to="/" className="nav-item">
+        <Link
+          to="/"
+          className={`nav-item ${currentURLNavItem === "/" ? "selected-link" : ""}`}
+          onClick={_ => {
+            setURLNavItem("/");
+          }}
+        >
           Home
         </Link>
-        <Link to="/unsubscribe" className="nav-item">
+        <Link
+          to="/unsubscribe"
+          className={`nav-item ${currentURLNavItem === "/unsubscribe" ? "selected-link" : ""}`}
+          onClick={_ => {
+            setURLNavItem("/unsubscribe");
+          }}
+        >
           Unsubscribe
         </Link>
-        <Link to="/stats" className="nav-item">
+        <Link
+          to="/stats"
+          className={`nav-item ${currentURLNavItem === "/stats" ? "selected-link" : ""}`}
+          onClick={_ => {
+            setURLNavItem("/stats");
+          }}
+        >
           Stats
         </Link>
       </nav>
