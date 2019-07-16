e9f1c66eddf350380e2c61a987acb833e3181b27
Yosefc <yosef.conn@gmail.com>
2019-07-16 15:10:13 +0300

    Custom select list is now clickable

--- client/src/components/CustomSelect.js
+++ client/src/components/CustomSelect.js
@@ -3,30 +3,38 @@ import { getTeams } from "../teams";
 
 const CustomSelect = () => {
   const [selectOpenState, setSelectOpenState] = useState(false);
+  const [selectedTeams, setSelectedTeams] = useState([]);
   return (
     <div>
       <input
+        tabIndex="0"
         type="text"
         id="custom-select"
         autoComplete="off"
+        value={selectedTeams}
         onClick={_ => setSelectOpenState(true)}
-        onBlur={_ => setSelectOpenState(false)}
       />
       <div
         id="custom-select-options"
         className={selectOpenState ? "custom-select-options-container shown" : "hidden"}
+        tabIndex="1"
+        onBlur={_ => setSelectOpenState(false)}
       >
-        <SelectOptions />
+        {getTeams().map(team => (
+          <li
+            className="custom-select-option"
+            key={team.id}
+            value={team.id}
+            onClick={e => setSelectedTeams([...selectedTeams, e.target.innerHTML])}
+          >
+            {team.name}
+          </li>
+        ))}
       </div>
     </div>
   );
 };
 
-const SelectOptions = () =>
-  getTeams().map(team => (
-    <li className="custom-select-option" key={team.id} value={team.id}>
-      {team.name}
-    </li>
-  ));
+// const SelectOptions = ()
 
 export default CustomSelect;
