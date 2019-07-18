d527f131a6009dc607cc13df853c5a1502bc2999
Yosefc <yosef.conn@gmail.com>
2019-07-18 10:37:14 +0300

    Made select options focused when parent input clicked

--- client/src/components/CustomSelect.js
+++ client/src/components/CustomSelect.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { getTeams } from "../teams";
 
 const CustomSelect = () => {
   const [selectOpenState, setSelectOpenState] = useState(false);
   const [selectedTeams, setSelectedTeams] = useState([]);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (selectOpenState) {
+      inputRef.current.focus();
+    }
+  }, [selectOpenState]);
 
   const SelectOptions = () =>
     getTeams().map(team => (
@@ -31,6 +38,7 @@ const CustomSelect = () => {
         id="custom-select-options"
         className={selectOpenState ? "custom-select-options-container shown" : "hidden"}
         tabIndex="1"
+        ref={inputRef}
         onBlur={_ => setSelectOpenState(false)}
       >
         <SelectOptions />
